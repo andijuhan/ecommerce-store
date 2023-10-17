b29bb873ec0412a873aa14bb3162b09c13e7cb0c
andijuhan <80870917+andijuhan@users.noreply.github.com>
2023-10-17 22:39:12 +0700

    update

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -18,7 +18,7 @@ const Summary = () => {
 
    const onCheckout = async () => {
       const response = await axios.post(
-         `${process.env.NEXT_PUBLIC_API_URL}/Checkout`,
+         `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
          {
             productIds: items.map((item) => item.id),
          }
