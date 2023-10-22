464451a9e23ff579f4311b7227ff4c609aef53c8
andijuhan <80870917+andijuhan@users.noreply.github.com>
2023-10-22 15:47:24 +0700

    update

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -21,7 +21,7 @@ const getProducts = async (query: Query): Promise<Product[]> => {
       },
    });
 
-   const res = await fetch(url, { next: { revalidate: 60 } });
+   const res = await fetch(url, { next: { revalidate: 0 } });
 
    return res.json();
 };
